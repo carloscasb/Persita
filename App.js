311a87742c4228db4785bc89e1ad5c2a23f5ba45
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-09-28 19:14:28 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
-//import React, {component} from "react";
 import React from "react";
 import {StyleSheet, View, Text} from 'react-native';
-//Importa o componente Não precisa ser o mesmo nome (Usei C1) e usa no return
-import C1 from './componentes/comp1' 
+//Importa os componentes necessarios - Não precisa ser o mesmo nome (Usei C1) e usa no return
+//import Caixas from './componentes/comp1' 
+import Caixas from './componentes/Caixas' 
 import Estilos from './estilos/Estilos.js';
+
+
+function fexibir(vp1){
+  if (vp1){
+        return(<Text>Vou aparecer ou não</Text>)
+
+  }else{
+    return(<Text>------</Text>)
+  }
+}
+
+/*  PODER SER ARRAYfunction
+const fexibir = (vp1) =>{
+  if (vp1){
+        return(<Text>Vou aparecer ou não</Text>)
+
+  }else{
+    return(<Text>------</Text>)
+  }
+}
+*/
+
 export  default  function App1(){
 return(
   <View style={Estilos.conteiner} >
-    <C1 curso="Cavalo" nota="10" />
-    <C1 curso="gato" nota="10" />
-    <C1 curso="cachorro" nota="10" />
-    <C1 curso="abelhas" nota="10"/>
+    <Caixas/>
     <Text style={Estilos.textoPadrao}>Vai dar tudo Certo</Text>
     <Text style={Estilos.textoTitulo}>Com a bença do Senhor</Text>
+    {fexibir (true)} 
     </View>
 );
 };
