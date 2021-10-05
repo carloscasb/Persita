60a692b3826b8778a0f25c7ced895b2e1b23100a
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-05 19:56:18 -0300

    05102021_app
    05102021_app

--- App.js
+++ App.js
@@ -1,65 +1,49 @@
-import React from "react";
-import {StyleSheet, View, Text, Image, Button, Alert} from 'react-native';
-//Importa os componentes necessarios - Não precisa ser o mesmo nome (Usei C1) e usa no return
-//import Caixas from './componentes/comp1' 
-import Caixas from './componentes/Caixas' 
-import Estilos from './estilos/Estilos.js';
+import React, {useState} from "react";
+import { StyleSheet,  View, Text} from 'react-native';
+import AsyncStorage from "@react-native-community/async-storage";
+//POSO CRIAR QUANTOS STORAGE QUISER
+import A_Curso from "@react-native-community/async-storage";
 
-
-const dobro=n=>n*2
-const soma=(n1,n2)=>n1+n2
-
-/*
-function fexibir(vp1){
-  if (vp1){
-        return(<Text>Vou aparecer ou não</Text>)
-
-  }else{
-    return(<Text>------</Text>)
-  }
+export default function App1(){
+  // CRIAR um state curso
+const [curso, setCurso]=useState(null)
+  // CRIAR uma função (arrayfuction) de Aramzenamento(GRAVAR) com o nome Armazenar, pasando chave e valor
+const Armazenar = (chave, valor)=>{
+  //ARMAZENA ITEM
+  AsyncStorage.setItem(chave, valor)
 }
-*/
-/*  PODER SER ARRAYfunction
-const fexibir = (vp1) =>{
-  if (vp1){
-        return(<Text>Vou aparecer ou não</Text>)
+// CRIAR uma função (arrayfuction) com o nome Buscar, para BUSCAR -- Quero um valor atraves de uma chave
+const Buscar = async (chave)=>{
+  //ESPERAR do meu asynsc storage o retorno desta chave 
+   const valor = await AsyncStorage.getItem(chave)
+   //COLOCAR O Valor recebido dentro do State curso
+   setCurso(valor)
+   // OU PODE SER UM return, depende do que quero fazer com o valor
+  // return valor
 
-  }else{
-    return(<Text>------</Text>)
-  }
-}
-*/
-
-function msg(){
-   Alert.alert ('MSG','puxando função')
 }
 
-export  default  function App1(){
-  let fexibir = true;
-return(
-  <View style={Estilos.conteiner}>
-    <Button
-      title="mostrar mensagem"
-      //Sua funcionalidade, no caso chamamos uma array fuction de alerta
-      onPress={()=>Alert.alert ('MSG','aprendendo a persitir')}
-    />
-    <Button
-      title="Puxar função"
-      //Sua funcionalidade, no caso chamamos uma array fuction de alerta
-      onPress={msg}
-    />
-   <Image source={require('./assets/hdc8.png')}
-    style={{ width: 250, resizeMode:'stretch' }}
-   />
-   <Text> {dobro (2)}</Text>
-   <Text> {soma (2,4)}</Text>
-    <Caixas exibir={fexibir} />
-    <Text style={Estilos.textoPadrao}>Vai dar tudo Certo</Text>
-    <Text style={Estilos.textoTitulo}>Com a bença do Senhor</Text>
-    {fexibir?<Text>Vou aparecer ou não</Text>:<Text>------</Text>}
-    {fexibir && <Text>Vou aparecer ou não</Text>}
-    </View>
-    
-);
-};
+ // VAMOS ARMAZENAR (chama a função que criamos --Armazenar ou AsyncStorage.setItem(chave, valor) )
+ Armazenar('01', 'React-native')
+ Armazenar('02', 'React-Natal')
+ Armazenar('03', 'Amazonas')
 
+ // VAMOS BUSCAR
+ Buscar('01')
+
+  return (
+        // VOU COLOCAR AQUI DENTRO DE UM TEXT
+      <View >
+
+
+      <Text>Curso: {curso}</Text> 
+      </View>
+
+  
+  );
+}
+
+
+const estilos = StyleSheet.create({
+
+});
