b6a6e743ead2c87959ededae4f2c0348fab290b4
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-05 19:54:29 -0300

    05102021_componentres
    05102021_componentres

--- componentes/Caixas.js
+++ componentes/Caixas.js
@@ -6,10 +6,22 @@ import { View } from 'react-native';
 export default function (props){
 
     return (
-      <View style={{width:'100%', height:300, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
-          <View style={{width:50, height:50, backgroundColor:'#0ff'}}></View>
-          <View style={{width:50, height:50, backgroundColor:'#f00'}}></View>
-          <View style={{width:50, height:50, backgroundColor:'#00f'}}></View>
+      <View style={{width:'100%', height:300, flexDirection:'column', justifyContent:'space-between'}}>
+
+        {
+      props.exibir ?
+          <View style={{flex:1,}}>
+          <View style={{ flexGrow:1 , backgroundColor:'#0ff'}}></View>
+          <View style={{ flexGrow:1 , backgroundColor:'#f00'}}></View>
+          <View style={{ flexGrow:1 , backgroundColor:'#00f'}}></View>
+          </View>
+          :
+          <View style={{flex:1,}}>
+          <View style={{ flexGrow:1 , backgroundColor:'#0ff'}}></View>
+          <View style={{ flexGrow:1 , backgroundColor:'#f00'}}></View>
+          <View style={{ flexGrow:1 , backgroundColor:'#000'}}></View>
+          </View>
+        }
 
       </View>
     );
---
+++ componentes/Globais.js
@@ -0,0 +1,7 @@
+class Globais{
+ static nome="Carlos"
+ static canal="HDC Tecnologia"
+ static num=2021
+ }
+
+ export default Globais;
